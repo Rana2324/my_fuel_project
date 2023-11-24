7c583b854d7a3f5c124c8190a8e9904ae19f7e20
Shihab Jamil <shihab.jamil@enkaizen.com>
2023-11-24 23:51:38 +0600

    all updated for contact

--- fuel/app/modules/module1/config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    '_root_' => 'module_views/index',  // Default route
-    '_404_'  => 'welcome/404',    // 404 route
-
-    'module1' => 'moduleC/index', // Main module route
-    'module1/route1' => 'moduleC/route1', // Route 1
-);
